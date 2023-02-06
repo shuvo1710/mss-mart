65091b1f0da733bfd037cccc701de47dbaea7535
shuvo1710 <mdshovo102002@gmail.com>
2023-02-06 22:02:50 +0600

    change indexcss

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Banner.css'
 import { Swiper, SwiperSlide } from "swiper/react";
-
-
 import "swiper/css";
 import "swiper/css/pagination";
-
-
 import { Pagination, Autoplay } from "swiper";
-
+import banner1 from "../../resource/banner1.jpg"
+import banner2 from "../../resource/banner2.jpg"
+import banner3 from "../../resource/banner3.jpg"
 
 const Banner = () => {
     return (
         <div>
             <Swiper
                 loop={true}
+
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
@@ -23,39 +22,57 @@ const Banner = () => {
                     clickable: true,
                 }}
                 navigation={true}
-                modules={[Pagination, Autoplay]}
+                modules={[Pagination,Autoplay]}
                 className="mySwiper bannerSwiper">
                 <SwiperSlide>
-                    <div className="relative bannerImg2" >
-                        <div className="absolute top-1/4">
-                            <div className="px-10">
-                                
-                                    <h3 className="text-3xl font-semibold">Dresses And Jumpsuits</h3>
-                                    <h1 className="text-6xl font-bold">Daisy Dress</h1>
-                                    <div className="my-10">
-                                        <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
-                                    </div>
-                           
+                    <div className='relative'>
+                        <div className=''>
+                            <img alt='' src={banner1} className="z-0"/>
+                        </div>
+                        <div className='absolute top-1/4 left-60 w-full z-10'>
+                            <h4 className="text-2xl font-medium uppercase">Fashion Trends</h4>
+                            <h3 className='text-4xl font-semibold my-2 uppercase'>The hoslist of </h3>
+                            <h1 className='text-5xl font-bold my-2 uppercase'>Summer</h1>
+                            <h6 className='text-lg'>The Biggest Spring/Summer 2023 Trends</h6>
+                            <div className="my-10">
+                                <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-
                 <SwiperSlide>
-                    <div className="relative bannerImg">
-                        <div className="absolute top-1/4">
-                            <div className="px-10" >
-                              
-                                    <h3 className="text-3xl font-semibold">Dresses And Jumpsuits</h3>
-                                    <h1 className="text-6xl font-bold">Daisy Dress</h1>
-                                    <div className="my-10">
-                                        <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
-                                    </div>
-                            
+                    <div className='relative'>
+                        <div className=''>
+                            <img alt='' src={banner2} className="z-0"/>
+                        </div>
+                        <div className='absolute top-1/4 left-60 w-full z-10'>
+                            <h4 className="text-2xl font-medium uppercase">Fashion Trends</h4>
+                            <h3 className='text-4xl font-semibold my-2 uppercase'>The hoslist of </h3>
+                            <h1 className='text-5xl font-bold my-2 uppercase'>Summer</h1>
+                            <h6 className='text-lg'>The Biggest Spring/Summer 2023 Trends</h6>
+                            <div className="my-10">
+                                <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
+                <SwiperSlide>
+                    <div className='relative'>
+                        <div className=''>
+                            <img alt='' src={banner3} className="z-0"/>
+                        </div>
+                        <div className='absolute top-1/4 left-60 w-full z-10'>
+                            <h4 className="text-2xl font-medium uppercase">New Collection</h4>
+                            <h3 className='text-4xl font-semibold my-2 uppercase'>Fashion </h3>
+                            <h1 className='text-5xl font-bold my-2 uppercase'>Glasses</h1>
+                            <h6 className='text-lg'>Best eyeglasses for woman to channel your inner clark kent</h6>
+                            <div className="my-10">
+                                <button className="px-4 py-2 text-black border border-black bg-white">Shop Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                
             </Swiper>
         </div>
     );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,72 +1,64 @@
 import React from 'react';
-import {GrFacebookOption} from 'react-icons/gr'
-import {BsTwitter} from 'react-icons/bs'
-import {AiOutlineInstagram} from 'react-icons/ai'
+import { GrFacebookOption } from 'react-icons/gr'
+import { BsTwitter } from 'react-icons/bs'
+import { AiOutlineInstagram } from 'react-icons/ai'
 
 const Footer = () => {
-    return (
-        <div className='container mx-auto mt-4 p-4 lg:p-0'>
-        <div className='md:flex justify-between mt-4 mb-10 items-center'>
-         <div>
-             <h1 className='text-4xl font-semibold mb-4'>Salehub</h1>
-             <p>We're available by phone <span className='text-2xl'>+123456789</span></p>
-             <p>info@example.com</p>
-             <h3 className="text-2xl font-semibold">Monday till Friday 10 to  6 EST</h3>
-         </div>
-
-         <div className='mt-3 md:mt-0'>
-             <h1 className='text-lg font-semibold mb-4'>GET MORE RESERVED ON:</h1>
-             <div className='flex gap-4 '>
-
-             <a href="/" className='text-lg font-semibold'><GrFacebookOption/></a>
-             <a href="/" className='text-lg font-semibold'><BsTwitter/></a>
-             <a href="/" className='text-lg font-semibold'><AiOutlineInstagram/></a>
-             </div>
-         </div>
-         
-     </div>
-
- 
- <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-20 mt-10">
- <div>
-   <span className="font-bold mb-3 text-black">About Us</span> 
-   <br />
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300  ">Branding</a> 
-   
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Design</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Marketing</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Advertisement</a>
- </div> 
- <div>
-   <span className="font-bold mb-3 text-black">ORDERS AND RETURNS </span> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">About us</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Contact</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Jobs</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Press kit</a>
- </div> 
- <div>
-   <span className="font-bold mb-3 text-black">MY ACCOUNT</span> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Terms of use</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Privacy policy</a> 
-   <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Cookie policy</a>
- </div> 
- <div>
-   <span className="font-bold mb-3 text-black">Newsletter</span> 
-   <div className="form-control w-80">
-     <label className="label">
-       <span className="label-text">Enter your email address</span>
-     </label> 
-     <div className="relative">
-       <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
-       
-       <button className="btn bg-black absolute top-0 right-0 rounded-l-none">Sign Me Up</button>
-     </div>
-   </div>
- </div>
-</div>
-
-</div>
-    );
+  return (
+    <div className='container mx-auto mt-4 p-4 lg:p-0'>
+      <div className='md:flex justify-between mt-4 mb-10 items-center'>
+        <div>
+          <h1 className='text-4xl font-semibold mb-4'>Salehub</h1>
+          <p>We're available by phone <span className='text-2xl'>+123456789</span></p>
+          <p>info@example.com</p>
+          <h3 className="text-2xl font-semibold">Monday till Friday 10 to  6 EST</h3>
+        </div>
+        <div className='mt-3 md:mt-0'>
+          <h1 className='text-lg font-semibold mb-4'>GET MORE RESERVED ON:</h1>
+          <div className='flex gap-4 '>
+            <a href="/" className='text-lg font-semibold'><GrFacebookOption /></a>
+            <a href="/" className='text-lg font-semibold'><BsTwitter /></a>
+            <a href="/" className='text-lg font-semibold'><AiOutlineInstagram /></a>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-20 mt-10">
+        <div>
+          <span className="font-bold mb-3 text-black">About Us</span>
+          <br />
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300  ">Branding</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Design</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Marketing</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Advertisement</a>
+        </div>
+        <div>
+          <span className="font-bold mb-3 text-black">ORDERS AND RETURNS </span>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">About us</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Contact</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Jobs</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Press kit</a>
+        </div>
+        <div>
+          <span className="font-bold mb-3 text-black">MY ACCOUNT</span>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Terms of use</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Privacy policy</a>
+          <a className="link link-hover block mt-2 hover:pl-3 duration-300 ">Cookie policy</a>
+        </div>
+        <div>
+          <span className="font-bold mb-3 text-black">Newsletter</span>
+          <div className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Enter your email address</span>
+            </label>
+            <div className="relative">
+              <input type="text" placeholder="username@site.com" className="input input-bordered border border-black w-full pr-16" />
+              <button className="btn bg-black text-white absolute top-0 right-0 rounded-l-none px-2 ">Sign Me Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
