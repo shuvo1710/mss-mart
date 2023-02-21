15a642f418e0c6601e6a46b991e9511dd4897776
shuvo1710 <mdshovo102002@gmail.com>
2023-02-21 10:09:27 +0600

    added bradcrumbs

--- src/CategoryContext/CategoryContext.js
+++ src/CategoryContext/CategoryContext.js
@@ -1,14 +1,26 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 export const Category=createContext()
 const CategoryContext = ({children}) => {
     const [modalData,setMOdalData] = useState([])
     const [categoryName, setCategoryName]=useState("")
     const [storeProduct, setStoreProduct]=useState([])
     useEffect(()=>{
-        fetch(`http://localhost:5000/allData?productType=${categoryName}`)
+        fetch(`http://localhost:5000/allProduct`)
         .then(res=> res.json())
         .then(data=> setStoreProduct(data))
     },[categoryName])
+    
+
+    // const {data:allProduct = [], isLoading} = useQuery({
+    //     queryKey: ['allProduct'],
+    //     queryFn: async ()=>{
+    //         const res = await fetch('http://localhost:5000/allProduct')
+    //         const data = await res.json()
+    //         return data;
+    //     }
+    // })
+    // console.log(allProduct)
 
     const userCategory={
         setCategoryName,
