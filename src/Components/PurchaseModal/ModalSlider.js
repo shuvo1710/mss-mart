ff9dc2d367139c43bf1cc78944c8aca63cbb8040
sajib ahmed <sajib7315@gmail.com>
2023-02-23 12:10:47 +0600

    adding

--- src/Components/PurchaseModal/ModalSlider.js
+++ src/Components/PurchaseModal/ModalSlider.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { EffectFade, Navigation, Pagination } from "swiper";
 
 const ModalSlider = ({images}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+         {
+            images?.map(image=> <SwiperSlide>
+            <img className="w-full" src={image.i} />
+          </SwiperSlide>)
+         }
+          
+        </Swiper>
+      </>
     );
 };
 
