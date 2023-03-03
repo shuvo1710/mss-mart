49063a09f5e589878cc2218b304258f15e336399
shuvo1710 <mdshovo102002@gmail.com>
2023-03-03 16:13:21 +0600

    auth

--- src/CategoryContext/AuthContext.js
+++ src/CategoryContext/AuthContext.js
@@ -1,24 +1,60 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from '../Components/firebase.confiq';
 
 
 export const UserContext = createContext()
 const auth = getAuth(app);
-const AuthContext = ({children}) => {
-    
-const registerWithWmailPass = (email,password) =>{
-    return createUserWithEmailAndPassword(auth,email,password)
-}
+const AuthContext = ({ children }) => {
+    const [user,SetUser] = useState('')
+    const [loader,SetLoader] = useState(true)
+
+    const registerWithEmailPass = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const logInWithEmailAndPassword = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const updateUser = (userName, userPhoto) => {
+        return updateProfile(auth.currentUser,{
+            displayName: userName,
+            photoURL: userPhoto
+        })
+    }
+
+    // useEffect(()=>{
+    //     const unsubscribed = onAuthStateChanged(auth,currentUser=>{
+    //         setUser(currentUser)
+    //         setLoading(false)
+            
+    //     })
+
+    //     return ()=>unsubscribed();
+    // },[])
+
+    useEffect(()=>{
+        const unsubscribed = onAuthStateChanged(auth,currentUser=>{
+            SetUser(currentUser)
+            SetLoader(false)
+        })
+        return ()=> unsubscribed();
+    },[]) 
 
 
 
 
 
-    const userInfo ={}
+
+    const userInfo = {
+        registerWithEmailPass,
+        logInWithEmailAndPassword,
+        updateUser
+
+    }
     return (
         <UserContext.Provider value={userInfo}>
-            
+            {children}
         </UserContext.Provider>
     );
 };
