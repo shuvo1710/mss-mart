065f628b2e08c1f835fc4d4b4e7164941672d77e
shuvo1710 <mdshovo102002@gmail.com>
2023-03-11 12:31:09 +0600

    added shuvo

--- src/Components/PrivateRouter/PrivateRouter.js
+++ src/Components/PrivateRouter/PrivateRouter.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import AuthContext, { UserContext } from '../../CategoryContext/AuthContext';
-import Loader from '../Loader/Loader';
+import Loader from '../Loder/Loader';
+
 
 const PrivateRouter = ({children}) => {
-    
-    const {user,loader} = UserContext(AuthContext)
+    const {user,loader} = useContext(UserContext)
     let location = useLocation()
 
     if(loader){
         return <Loader></Loader>
     }
 
-    if(user?.uid){
+    if(user){
         return children;
     }
    else{
